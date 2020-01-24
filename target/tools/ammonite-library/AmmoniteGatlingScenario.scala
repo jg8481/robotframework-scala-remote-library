9ab4998dc1e421f34478b46285afd1ae66c62a54
Joshua Gorospe <>
2020-01-24 01:34:20 -0500

    Add a new scala-remote-library-server-1.0.jar file to the docker-robotframework-scala package

--- target/tools/ammonite-library/AmmoniteGatlingScenario.scala
+++ target/tools/ammonite-library/AmmoniteGatlingScenario.scala
@@ -4,60 +4,27 @@ import $ivy.`io.gatling.highcharts:gatling-charts-highcharts:3.1.2`
 import $ivy.`ch.qos.logback:logback-classic:1.2.3`
 
 
-import io.gatling.app.Gatling
-import io.gatling.core.config.GatlingPropertiesBuilder
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
-import org.slf4j.{LoggerFactory, Logger}
+import scala.util.Random
 
+class BasicSimulation extends Simulation {
 
-val rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME).asInstanceOf[ch.qos.logback.classic.Logger]
-rootLogger.setLevel(ch.qos.logback.classic.Level.ERROR)
+  val httpProtocol = http
+    .baseUrl("http://computer-database.gatling.io")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+      .get("/"))
+    .pause(5)
 
-class RobotFrameworkScalaGatlingScenario extends Simulation {
-
-  def gatlingScenario =
-    scenario("Simple load").during(30 seconds) {
-      exec(
-        http("get200")
-          .get("/")
-          .check(status.is(200)) )
-        .pause(1000 milliseconds, 2000 milliseconds)
-        .exec(
-          http("get401")
-            .get("/status/401")
-            .check(status.is(401)) )
-        .pause(2000 milliseconds, 4000 milliseconds)
-    }
-
-  def gatlingConfiguration = {
-    http
-      .baseUrl("https://httpbin.org/")
-      .userAgentHeader("curl/7.58.0")
-      .disableFollowRedirect
-  }
-
-  def gatlingAssertions = List(
-    global.responseTime.mean.lt(90),
-    global.successfulRequests.percent.gt(90)
-  )
-
-
-  setUp(gatlingScenario.inject(rampUsers(10) during (10 seconds)))
-    .protocols(gatlingConfiguration)
-    .assertions(gatlingAssertions)
-
+  setUp( 
+    scn.inject(atOnceUsers(1000))
+  ).protocols(httpProtocol)
 }
-
-
-
-val propertiesToBuild =
-  new GatlingPropertiesBuilder()
-    .simulationClass(classOf[RobotFrameworkScalaGatlingScenario].getName)
-    .resultsDirectory("/tmp")
-    //.noReports()
-    .build
-
-Gatling.fromMap(propertiesToBuild)
