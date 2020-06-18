de430c87e27b01e1432f8010b78c1c5312c9bbbd
Joshua Gorospe <>
2020-06-18 19:38:17 -0400

    Update build-remote-server-and-gatling-jars.sh and docker-compose.yml

--- build-remote-server-and-gatling-jars.sh
+++ build-remote-server-and-gatling-jars.sh
@@ -12,4 +12,10 @@ cd ./target/tools/scala-remote-library-gatling-jar
 mvn clean
 mvn dependency:copy-dependencies &&
 mvn package
+cd ../../../
+rm -rf ./docker-robotframework-scala/application-under-test/tools/scala-remote-library-gatling-jar-1.0.0.jar
+rm -rf ./docker-robotframework-scala/application-under-test/tools/scala-remote-library-server-1.0.jar
+cp ./target/tools/scala-remote-library-gatling-jar/target/scala-remote-library-gatling-jar-1.0.0.jar ./docker-robotframework-scala/application-under-test/tools/scala-remote-library-gatling-jar-1.0.0.jar
+cp ./target/scala-remote-library-server-1.0.jar ./docker-robotframework-scala/application-under-test/tools/scala-remote-library-server-1.0.jar
+exit
 exit
